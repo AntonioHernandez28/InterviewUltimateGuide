e5272356c77704006d2eb87bff02240b1e7dae54
Antonio <a01382469@itesm.mx>
2020-09-27 04:18:06 -0500

    DP first problem added

---
+++ DP/DP.cpp
@@ -0,0 +1,22 @@
+#include<iostream> 
+#include<vector>
+using namespace std; 
+
+// Problema 1: Mejorar el algoritmo recursivo de Fibonacci
+// Algoritmo Fib SIN DP
+int Fib(int n){
+    if(n < 2) return 1; 
+    return Fib(n - 1) + Fib(n - 2);
+}
+// Algoritmo con DP
+vector<int> DP(1000, -1); 
+int FibWithDP(int n){
+    DP[0] = DP[1] = 1; 
+    if(DP[n] == -1) DP[n] = FibWithDP(n - 1) + FibWithDP(n - 2); 
+    return DP[n]; 
+}
+
+int main(){
+    cout << "DP: " << FibWithDP(45) << endl; 
+    cout << "NP: " << Fib(45) << endl;
+}
